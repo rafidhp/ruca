4ffae11afcccb6fb06556e6e0a52878e9524e748
rafidhp <rafiislamipasha27@gmail.com>
2025-07-28 02:56:33 +0700

    feat: add eduvid dummy data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             PsikologSeeder::class,
             CategorySeeder::class,
+            EduVidSeeder::class,
         ]);
     }
 }
--- database/seeders/EduVidSeeder.php
+++ database/seeders/EduVidSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\EduVid;
 
 class EduVidSeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class EduVidSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $eduvids = [
+            [
+                'title' => 'Test video 1',
+                'vid_link' => 'https://youtu.be/vMADTUm7BbU?si=1iikSyPW2Lsg5xZQ',
+                'description' => 'This is a test video description',
+                'user_id' => 3,
+                'category_id' => 2,
+            ],
+        ];
+
+        foreach ($eduvids as $eduvid) {
+            Eduvid::create($eduvid);
+        }
     }
 }
